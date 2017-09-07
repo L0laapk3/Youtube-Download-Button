14d319fb11aa108070563b61173c110735e645f5
L0laapk3 <L0laapk3@gmail.com>
2017-09-07 16:45:08 +0200

    updated update link

--- download.user.js
+++ download.user.js
@@ -3,8 +3,8 @@
 // @version      0.2
 // @author       L0laapk3
 // @match        https://www.youtube.com/watch?v=*
-// @require 	 http://code.jquery.com/jquery-1.12.4.min.js
-// @downloadURL	 https://github.com/L0laapk3/Youtube-Download-Button/raw/master/download.user.js
+// @require      http://code.jquery.com/jquery-1.12.4.min.js
+// @downloadURL  https://rawgit.com/L0laapk3/Youtube-Download-Button/master/download.user.js
 // @grant        none
 // ==/UserScript==
 
