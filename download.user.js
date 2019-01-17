090b0bc8c08a6dd84d0bd36af558f02dc0f6a994
L0laapk3 <L0laapk3@gmail.com>
2019-01-17 06:17:19 +0100

    added download button directly to google search result

--- download.user.js
+++ download.user.js
@@ -1,8 +1,202 @@
 // ==UserScript==
 // @name         Youtube Download button
-// @version      4.2.1
+// @version      5.0.0
 // @author       L0laapk3
 // @match        *://www.youtube.com/*
+// @match        *://www.google.com/search*
+// @match        *://www.google.ad/search*
+// @match        *://www.google.ae/search*
+// @match        *://www.google.com.af/search*
+// @match        *://www.google.com.ag/search*
+// @match        *://www.google.com.ai/search*
+// @match        *://www.google.al/search*
+// @match        *://www.google.am/search*
+// @match        *://www.google.co.ao/search*
+// @match        *://www.google.com.ar/search*
+// @match        *://www.google.as/search*
+// @match        *://www.google.at/search*
+// @match        *://www.google.com.au/search*
+// @match        *://www.google.az/search*
+// @match        *://www.google.ba/search*
+// @match        *://www.google.com.bd/search*
+// @match        *://www.google.be/search*
+// @match        *://www.google.bf/search*
+// @match        *://www.google.bg/search*
+// @match        *://www.google.com.bh/search*
+// @match        *://www.google.bi/search*
+// @match        *://www.google.bj/search*
+// @match        *://www.google.com.bn/search*
+// @match        *://www.google.com.bo/search*
+// @match        *://www.google.com.br/search*
+// @match        *://www.google.bs/search*
+// @match        *://www.google.bt/search*
+// @match        *://www.google.co.bw/search*
+// @match        *://www.google.by/search*
+// @match        *://www.google.com.bz/search*
+// @match        *://www.google.ca/search*
+// @match        *://www.google.cd/search*
+// @match        *://www.google.cf/search*
+// @match        *://www.google.cg/search*
+// @match        *://www.google.ch/search*
+// @match        *://www.google.ci/search*
+// @match        *://www.google.co.ck/search*
+// @match        *://www.google.cl/search*
+// @match        *://www.google.cm/search*
+// @match        *://www.google.cn/search*
+// @match        *://www.google.com.co/search*
+// @match        *://www.google.co.cr/search*
+// @match        *://www.google.com.cu/search*
+// @match        *://www.google.cv/search*
+// @match        *://www.google.com.cy/search*
+// @match        *://www.google.cz/search*
+// @match        *://www.google.de/search*
+// @match        *://www.google.dj/search*
+// @match        *://www.google.dk/search*
+// @match        *://www.google.dm/search*
+// @match        *://www.google.com.do/search*
+// @match        *://www.google.dz/search*
+// @match        *://www.google.com.ec/search*
+// @match        *://www.google.ee/search*
+// @match        *://www.google.com.eg/search*
+// @match        *://www.google.es/search*
+// @match        *://www.google.com.et/search*
+// @match        *://www.google.fi/search*
+// @match        *://www.google.com.fj/search*
+// @match        *://www.google.fm/search*
+// @match        *://www.google.fr/search*
+// @match        *://www.google.ga/search*
+// @match        *://www.google.ge/search*
+// @match        *://www.google.gg/search*
+// @match        *://www.google.com.gh/search*
+// @match        *://www.google.com.gi/search*
+// @match        *://www.google.gl/search*
+// @match        *://www.google.gm/search*
+// @match        *://www.google.gp/search*
+// @match        *://www.google.gr/search*
+// @match        *://www.google.com.gt/search*
+// @match        *://www.google.gy/search*
+// @match        *://www.google.com.hk/search*
+// @match        *://www.google.hn/search*
+// @match        *://www.google.hr/search*
+// @match        *://www.google.ht/search*
+// @match        *://www.google.hu/search*
+// @match        *://www.google.co.id/search*
+// @match        *://www.google.ie/search*
+// @match        *://www.google.co.il/search*
+// @match        *://www.google.im/search*
+// @match        *://www.google.co.in/search*
+// @match        *://www.google.iq/search*
+// @match        *://www.google.is/search*
+// @match        *://www.google.it/search*
+// @match        *://www.google.je/search*
+// @match        *://www.google.com.jm/search*
+// @match        *://www.google.jo/search*
+// @match        *://www.google.co.jp/search*
+// @match        *://www.google.co.ke/search*
+// @match        *://www.google.com.kh/search*
+// @match        *://www.google.ki/search*
+// @match        *://www.google.kg/search*
+// @match        *://www.google.co.kr/search*
+// @match        *://www.google.com.kw/search*
+// @match        *://www.google.kz/search*
+// @match        *://www.google.la/search*
+// @match        *://www.google.com.lb/search*
+// @match        *://www.google.li/search*
+// @match        *://www.google.lk/search*
+// @match        *://www.google.co.ls/search*
+// @match        *://www.google.lt/search*
+// @match        *://www.google.lu/search*
+// @match        *://www.google.lv/search*
+// @match        *://www.google.com.ly/search*
+// @match        *://www.google.co.ma/search*
+// @match        *://www.google.md/search*
+// @match        *://www.google.me/search*
+// @match        *://www.google.mg/search*
+// @match        *://www.google.mk/search*
+// @match        *://www.google.ml/search*
+// @match        *://www.google.com.mm/search*
+// @match        *://www.google.mn/search*
+// @match        *://www.google.ms/search*
+// @match        *://www.google.com.mt/search*
+// @match        *://www.google.mu/search*
+// @match        *://www.google.mv/search*
+// @match        *://www.google.mw/search*
+// @match        *://www.google.com.mx/search*
+// @match        *://www.google.com.my/search*
+// @match        *://www.google.co.mz/search*
+// @match        *://www.google.com.na/search*
+// @match        *://www.google.com.nf/search*
+// @match        *://www.google.com.ng/search*
+// @match        *://www.google.com.ni/search*
+// @match        *://www.google.ne/search*
+// @match        *://www.google.nl/search*
+// @match        *://www.google.no/search*
+// @match        *://www.google.com.np/search*
+// @match        *://www.google.nr/search*
+// @match        *://www.google.nu/search*
+// @match        *://www.google.co.nz/search*
+// @match        *://www.google.com.om/search*
+// @match        *://www.google.com.pa/search*
+// @match        *://www.google.com.pe/search*
+// @match        *://www.google.com.pg/search*
+// @match        *://www.google.com.ph/search*
+// @match        *://www.google.com.pk/search*
+// @match        *://www.google.pl/search*
+// @match        *://www.google.pn/search*
+// @match        *://www.google.com.pr/search*
+// @match        *://www.google.ps/search*
+// @match        *://www.google.pt/search*
+// @match        *://www.google.com.py/search*
+// @match        *://www.google.com.qa/search*
+// @match        *://www.google.ro/search*
+// @match        *://www.google.ru/search*
+// @match        *://www.google.rw/search*
+// @match        *://www.google.com.sa/search*
+// @match        *://www.google.com.sb/search*
+// @match        *://www.google.sc/search*
+// @match        *://www.google.se/search*
+// @match        *://www.google.com.sg/search*
+// @match        *://www.google.sh/search*
+// @match        *://www.google.si/search*
+// @match        *://www.google.sk/search*
+// @match        *://www.google.com.sl/search*
+// @match        *://www.google.sn/search*
+// @match        *://www.google.so/search*
+// @match        *://www.google.sm/search*
+// @match        *://www.google.sr/search*
+// @match        *://www.google.st/search*
+// @match        *://www.google.com.sv/search*
+// @match        *://www.google.td/search*
+// @match        *://www.google.tg/search*
+// @match        *://www.google.co.th/search*
+// @match        *://www.google.com.tj/search*
+// @match        *://www.google.tk/search*
+// @match        *://www.google.tl/search*
+// @match        *://www.google.tm/search*
+// @match        *://www.google.tn/search*
+// @match        *://www.google.to/search*
+// @match        *://www.google.com.tr/search*
+// @match        *://www.google.tt/search*
+// @match        *://www.google.com.tw/search*
+// @match        *://www.google.co.tz/search*
+// @match        *://www.google.com.ua/search*
+// @match        *://www.google.co.ug/search*
+// @match        *://www.google.co.uk/search*
+// @match        *://www.google.com.uy/search*
+// @match        *://www.google.co.uz/search*
+// @match        *://www.google.com.vc/search*
+// @match        *://www.google.co.ve/search*
+// @match        *://www.google.vg/search*
+// @match        *://www.google.co.vi/search*
+// @match        *://www.google.com.vn/search*
+// @match        *://www.google.vu/search*
+// @match        *://www.google.ws/search*
+// @match        *://www.google.rs/search*
+// @match        *://www.google.co.za/search*
+// @match        *://www.google.co.zm/search*
+// @match        *://www.google.co.zw/search*
+// @match        *://www.google.cat/search*
+
 // @require      http://code.jquery.com/jquery-1.12.4.min.js
 // @require      https://cdn.rawgit.com/meetselva/attrchange/master/js/attrchange.js
 // @updateURL    https://rawgit.com/L0laapk3/Youtube-Download-Button/master/download.user.js
@@ -16,6 +210,8 @@
 
 
 (function() {
+    console.log(typeof GM.xmlHttpRequest);
+
     var button, subButton;
     var topPos;
 
@@ -23,15 +219,67 @@
 
     var lastId;
 
+    var youtube = document.URL.indexOf("google") < 0;
     function check() {
-        if (location.href == lasturl) return;
-        lasturl = location.href;
-        if (lasturl.indexOf("watch?v=")) init();
+        console.log("check");
+        if (youtube) {
+            if (location.href == lasturl) return;
+            lasturl = location.href;
+            if (lasturl.indexOf("watch?v=")) initYT();
+        } else if (document.getElementById("resultStats")) initG();
     }
+
     var checkInt = setInterval(check, 50);
+    var node;
+    if (!youtube) {
+        console.log("google");
+
+    } else {
+        console.log("youtube");
+    }
 
 
-    function init() {
+
+    node = document.createElement('style');
+    if (youtube) node.innerHTML =
+        "#downloadbutton { transition: margin 0.2s; margin: auto; user-select: none; white-space: nowrap; text-transform: uppercase; background-color: orange;color: white;border: solid 2px orange;border-radius: 2px;cursor: pointer; font-size: 1.4rem; letter-spacing: .007px; height: 33px; line-height: 33px; padding: 0 15px; font-weight: 500; z-index: 1; position: relative; }" +
+        "#downloadtype { margin-left: 4px; color: rgba(255, 255, 255, 0.85); }" +
+        "#downloadtype > span { font-size: 1.05em; }" +
+        "#downloadprogress { border-bottom: 2px; border-bottom-left-radius: 2px; border-bottom-right-radius: 2px; position: absolute ;bottom: -2px; width: 100%; left: -2px; overflow: hidden; padding: 0 4px 0 0; height: 2px; }" +
+        "#downloadprogressbar { background-color: #4a8df8; height: 2px; width: 0; position: absolute; }" +
+        "#downloadbutton.idle #downloadprogress:before, #downloadbutton.idle #downloadprogress:after { position: absolute; background: #4a8df8; height: 100%; content: ''; }" +
+        "#downloadbutton.idle #downloadprogress:before { animation: increase 2s infinite; } #downloadbutton.idle #downloadprogress:after { animation: decrease 2s 0.5s infinite; }" +
+        "@keyframes increase { from { left: -5%; width: 5%; } to { left: 130%; width: 100%;} } @keyframes decrease { from { left: -80%; width: 80%; } to { left: 110%; width: 10%;} }";
+    else node.innerHTML =
+        "@import url('https://fonts.googleapis.com/css?family=Roboto:500');" +
+        "#downloadbutton { float: right; font-family: Roboto, Arial, sans-serif; transition: margin 0.2s; margin: auto; user-select: none; white-space: nowrap; text-transform: uppercase; background-color: orange;color: white;border: solid 2px orange;border-radius: 2px;cursor: pointer; font-size: 14px; letter-spacing: .007px; height: 33px; line-height: 33px; padding: 0 15px; font-weight: 500; z-index: 1; position: relative; }" +
+        "#downloadtype { margin-left: 4px; color: rgba(255, 255, 255, 0.85); }" +
+        "#downloadtype > span { font-size: 1.05em; }" +
+        "#downloadprogress { border-bottom: 2px; border-bottom-left-radius: 2px; border-bottom-right-radius: 2px; position: absolute ;bottom: -2px; width: 100%; left: -2px; overflow: hidden; padding: 0 4px 0 0; height: 2px; }" +
+        "#downloadprogressbar { background-color: #4a8df8; height: 2px; width: 0; position: absolute; }" +
+        "#downloadbutton.idle #downloadprogress:before, #downloadbutton.idle #downloadprogress:after { position: absolute; background: #4a8df8; height: 100%; content: ''; }" +
+        "#downloadbutton.idle #downloadprogress:before { animation: increase 2s infinite; } #downloadbutton.idle #downloadprogress:after { animation: decrease 2s 0.5s infinite; }" +
+        "@keyframes increase { from { left: -5%; width: 5%; } to { left: 130%; width: 100%;} } @keyframes decrease { from { left: -80%; width: 80%; } to { left: 110%; width: 10%;} }";
+
+    document.head.append(node);
+
+
+    function initG() {
+        let YTThumbList = [].slice.call(document.querySelectorAll('a[href*="www.youtube.com"] img')).sort((a, b) => b.height*b.width-a.height*a.width);
+        clearInterval(checkInt);
+        if (!YTThumbList.length) return;
+        let url = YTThumbList[0].parentNode.href;
+        let title = document.querySelectorAll('a[href*="www.youtube.com"] h3')[0].innerText.replace(/ - youtube$/gi, '');
+        // <span>⯆</span>
+
+        if (button) button.remove();
+        button = $('<div id="downloadbutton">Download <span id="downloadtype">MP3<span>&#9660;</span></span><div id="downloadprogress"><div id="downloadprogressbar"></div></div></div>');
+        button.one("click", () => download(url.split("=")[1], title));
+        button.prependTo(YTThumbList[0].parentNode.parentNode.parentNode.children[1]);
+    }
+
+
+    function initYT() {
         if (lastId && button && lastId == button.closest("ytd-watch, ytd-watch-flexy").attr("video-id")) return;
         console.warn("init!", lastId);
         subButton = topPos = undefined;
@@ -40,8 +288,8 @@
         if (location.href.indexOf("watch") == -1) return;
         clearInterval(checkInt);
         // <span>⯆</span>
-        button = $('<div id="downloadbutton">Download <span id="downloadtype">MP3</span><div id="downloadprogress"><div id="downloadprogressbar"></div></div></div>');
-        button.one("click", download);
+        button = $('<div id="downloadbutton">Download <span id="downloadtype">MP3<span>&#9660;</span></span><div id="downloadprogress"><div id="downloadprogressbar"></div></div></div>');
+        button.one("click", () => download(button.closest("ytd-watch, ytd-watch-flexy").attr("video-id"), button.closest("[id='primary-inner']").find("[id='info-contents'] .title").text().replace(/[\\\/:*?<>|]|^ | $|\.$|\n\n|\r/g, '')));
         waitForDiv();
         var url = window.location.href;
         /*setTimeout(function() {
@@ -51,18 +299,6 @@
             }
         }, 1000);*/
     }
-
-    var node = document.createElement('style');
-    node.innerHTML = "#downloadbutton { transition: margin 0.2s; margin: auto; user-select: none; white-space: nowrap; text-transform: uppercase; background-color: orange;color: white;border: solid 2px orange;border-radius: 2px;cursor: pointer; font-size: 1.4rem; letter-spacing: .007px; height: 33px; line-height: 33px; padding: 0 15px; font-weight: 500; z-index: 1; position: relative; }" +
-                     "#downloadtype { margin-left: 4px; color: rgba(255, 255, 255, 0.85); }" +
-                     "#downloadtype > span { font-size: 1.05em; }" +
-                     "#downloadprogress { border-bottom: 2px; border-bottom-left-radius: 2px; border-bottom-right-radius: 2px; position: absolute ;bottom: -2px; width: 100%; left: -2px; overflow: hidden; padding: 0 4px 0 0; height: 2px; }" +
-                     "#downloadprogressbar { background-color: #4a8df8; height: 2px; width: 0; position: absolute; }" +
-                     "#downloadbutton.idle #downloadprogress:before, #downloadbutton.idle #downloadprogress:after { position: absolute; background: #4a8df8; height: 100%; content: ''; }" +
-                     "#downloadbutton.idle #downloadprogress:before { animation: increase 2s infinite; } #downloadbutton.idle #downloadprogress:after { animation: decrease 2s 0.5s infinite; }" +
-                     "@keyframes increase { from { left: -5%; width: 5%; } to { left: 130%; width: 100%;} } @keyframes decrease { from { left: -80%; width: 80%; } to { left: 110%; width: 10%;} }";
-
-    document.head.append(node);
 
     var happened = false;
     function waitForDiv(i) {
@@ -86,9 +322,9 @@
                 happened = true;
                 setTimeout(function() { happened = false; }, 0);
                 if ((!button || !button.offset() || !(button.offset().top - $("body").offset().top)) && location.href.indexOf("watch") > -1)
-                    init();
+                    initYT();
                 else if (lastId && lastId != button.closest("ytd-watch, ytd-watch-flexy").attr("video-id"))
-                    init();
+                    initYT();
                 else
                     hasScrolled();
             }});
@@ -97,15 +333,13 @@
     }
 
 
-    function download() {
+    function download(id, title) {
 
 
 
         button.addClass("idle")[0].style.cursor = "progress";
-        var id = button.closest("ytd-watch, ytd-watch-flexy").attr("video-id");
         console.assert(id && id.length > 2, "could not extract youtube id");
         var url = "https://www.youtube.com/watch?v=" + id;
-        var title = button.closest("[id='primary-inner']").find("[id='info-contents'] .title").text().replace(/[\\\/:*?<>|]|^ | $|\.$|\n\n|\r/g, '');
         console.assert(title && title.length > 0, "could not extract youtube title");
 
 
@@ -211,7 +445,7 @@
         for (const key of Object.keys(errors))
             string += "\n" + key + ": " + errors[key];
         alert(string);
-        init();
+        youtube ? initYT() : initG();
     }
 
 
@@ -307,7 +541,7 @@
 
                         var i = 0, lastProgress = 0;
                         function checkStatus() {
-                            GM_xmlhttpRequest({
+                            GM.xmlHttpRequest({
                                 method: "GET",
                                 url: statusUrl,
                                 onload: function(a) {
@@ -359,7 +593,7 @@
             name: "onlinevideoconverter.com",
             priority: 1,
             downloadFn: function(url, title, id, finish, error, progress) {
-                GM_xmlhttpRequest({
+                GM.xmlHttpRequest({
                     method: "POST",
                     url: "https://www3.onlinevideoconverter.com/webservice",
                     data: {
